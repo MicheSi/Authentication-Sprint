b8fa6564aa1032c18a050d8d90951fdcf43b4831
mlazzell17@gmail.com <MicheSi>
2020-02-28 10:34:32 -0700

    added tests

---
+++ jokes/joke.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const server = require('../api/server');
+
+describe('jokes router', function () {
+    it('able to run tests', function () {
+        expect(true).toBe(true);
+    });
+
+})
